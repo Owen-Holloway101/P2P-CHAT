8c3fd899f0103b84d18ed97fa27e7723c164b557
Owen Holloway <owen.holloway101@gmail.com>
2013-08-30 19:47:05 +1000

    Prep for rewrite

--- Main/src/tk/zeryter/p2pchat/P2PChatMain.java
+++ Main/src/tk/zeryter/p2pchat/P2PChatMain.java
@@ -2,9 +2,6 @@ package tk.zeryter.p2pchat;
 
 public class P2PChatMain {
 
-    public static Network net = new Network();
-    public static DiscoveryNet discnet = new DiscoveryNet();
-
     public static void main(String[] args) {
         System.out.println("Owen Holloway, GYC");
         System.out.println(" _____               _   ____         __ _   \n" +
@@ -14,19 +11,6 @@ public class P2PChatMain {
                            "/____\\___|_|   \\__, |\\__|____/ \\___/|_|  \\__|\n" +
                            "               |___/                         ");
         System.out.println("www.zeryter.tk");
-
-
-       // new Thread(new Window()).start();
-
-        net.setPort(5555);
-        net.setPACKETSIZE(100);
-        net.setRunning(true);
-        new Thread(net).start();
-
-        discnet.setPort(5555);
-        discnet.setPACKETSIZE(100);
-        discnet.setRunning(true);
-        new Thread(discnet).start();
 
 
     }
