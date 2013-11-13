de577d42527afba397b9df86896a8010db6efdf0
Owen Holloway <owen.holloway101@gmail.com>
2013-11-13 16:23:16 +1100

    init commit and base file creation

---
+++ tk/zeryter/p2pchat/P2PChatMain.java
@@ -0,0 +1,15 @@
+package tk.zeryter.p2pchat;
+
+/**
+ * Owen Holloway
+ * ZerytSoft
+ * Date: 13/11/13
+ */
+
+public class P2PChatMain {
+
+    public static void main(String args[]) {
+
+    }
+
+}
